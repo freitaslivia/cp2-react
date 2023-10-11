6de102af0e538ff5bb8732ef4f6bf88dfc7e22dd
VitorKubica <vitor05kubica12@gmail.com>
2023-10-11 11:19:40 -0300

    get da lista de produtos

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -1,6 +1,67 @@
+import { GrFormEdit as Editar } from "react-icons/gr";
+import { RiDeleteBin2Fill as Excluir } from "react-icons/ri";
+import style from "./Produtos.module.css";
+import { useEffect, useState } from "react";
+import "./Produtos.scss";
 
 export default function Produtos() {
+  document.title = "Produtos";
+
+  const [listaProdutoExterna, setListaProdutoExterna] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/produtos", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setListaProdutoExterna(data);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+
   return (
-    <div>Produtos</div>
-  )
+    <div>
+      <h1>LISTA DE PRODUTOS</h1>
+
+      <table className={style.tblEstilo}>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>NOME</th>
+            <th>DESCRIÇÃO</th>
+            <th>PREÇO</th>
+            <th>EDITAR / EXCLUIR</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listaProdutoExterna.map((item, indice) => (
+            <tr key={indice} className={style.tblLine}>
+              <td>{item.id}</td>
+              <td>{item.nome}</td>
+              <td>{item.desc}</td>
+              <td>{item.valor}</td>
+              <td>
+                <button>
+                  <Editar />
+                </button>
+                <button>
+                  <Excluir />
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+        <tfoot>
+          <tr>
+            <td colSpan={4}>PRODUTOS LINDOS</td>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+  );
 }
