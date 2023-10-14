543dfeed1a4bfa1039af81ee2f7dc6e28a8b552d
vcamargo04 <viniciusac2005@Hotmail.com>
2023-10-14 16:23:19 -0300

    Adicionado o import da App.css.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
 import Cabecalho from "./components/Cabecalho/Cabecalho";
 import Rodape from "./components/Rodape/Rodape";
+import "./App.css";
 
 export default function App() {
   return (
